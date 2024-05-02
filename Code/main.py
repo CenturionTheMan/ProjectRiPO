3ff97979d7ae418b8b837c625069493f17f06b34
Michał Zychowicz <zychomichal@gmail.com>
2024-05-02 22:33:55 +0200

    Wykrywanie co x klatek

--- Code/main.py
+++ Code/main.py
@@ -1,12 +1,14 @@
 import math
 
 import cv2
-
+import torch
 from video_handler import VideoHandler
 from line_drawer import draw_parking_line
 from objects_detection import ObjectsDetector
 
 if __name__ == '__main__':
+    # torch.set_default_device("mps")
+
     video_handler = VideoHandler('../Videos/2024-03-30_14-15-32-back.mp4')
     objects_detector = ObjectsDetector()
 
@@ -15,15 +17,26 @@ if __name__ == '__main__':
     frame = video_handler.get_next_frame(frame_size)
     height = frame.shape[0]
     width = frame.shape[1]
+    frame_number = 0
+    results = None
 
     while frame is not None:
-        objects_detector.detect_cars_yolo5(frame)
+        if frame_number % 10 == 0:
+            results = objects_detector.detect_cars_yolo5(frame)
+            frame_number = 0
+        frame_number += 1
 
-        draw_parking_line(frame, pivot=(width * 0.15, 0), angle_deg=15, length=700, max_thickness=30, min_thickness=10, rgb=(255, 255, 255))
-        draw_parking_line(frame, pivot=(width * 0.85, 0), angle_deg=-15, length=700, max_thickness=30, min_thickness=10, rgb=(255, 255, 255))
+        for res in results:
+            label = int(res[-1])
+            if label == 2:  # Label for car in COCO dataset
+                # Draw bounding box
+                x_min, y_min, x_max, y_max, conf = map(int, res[:5])
+                cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)
+
+        # draw_parking_line(frame, pivot=(width * 0.15, 0), angle_deg=15, length=700, max_thickness=30, min_thickness=10, rgb=(255, 255, 255))
+        # draw_parking_line(frame, pivot=(width * 0.85, 0), angle_deg=-15, length=700, max_thickness=30, min_thickness=10, rgb=(255, 255, 255))
 
         video_handler.display_frame(frame)
         frame = video_handler.get_next_frame(frame_size)
         if cv2.waitKey(1) == 27:
             break
-
--- Code/objects_detection.py
+++ Code/objects_detection.py
@@ -1,4 +1,4 @@
-import yolov5
+# import yolov5
 import numpy as np
 import torch
 import cv2
@@ -7,19 +7,24 @@ import cv2
 class ObjectsDetector:
     def __init__(self):
         # load pretrained model
-        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5n', pretrained=True)
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
         self.model.conf = 0.10  # NMS confidence threshold
         self.model.iou = 0.45  # NMS IoU threshold
         self.model.agnostic = False  # NMS class-agnostic
         self.model.multi_label = False  # NMS multiple labels per box
         self.model.max_det = 10  # maximum number of detections per image
+        # print(next(self.model.parameters().is_cuda))
+
+        # mps_device = torch.device("mps")
+        # self.model.to(mps_device)
 
     def detect_cars_yolo5(self, frame, color=(255, 0, 0), thickness=2):
         results = self.model(frame)
-        for res in results.xyxy[0]:
-            label = int(res[-1])
-            if label == 2:  # Label for car in COCO dataset
-                # Draw bounding box
-                x_min, y_min, x_max, y_max, conf = map(int, res[:5])
-                cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color, thickness)
+        return results.xyxy[0]
+        # for res in results.xyxy[0]:
+        #     label = int(res[-1])
+        #     if label == 2:  # Label for car in COCO dataset
+        #         # Draw bounding box
+        #         x_min, y_min, x_max, y_max, conf = map(int, res[:5])
+        #         cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color, thickness)
 
