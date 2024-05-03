3388ebef43a10e031841cfe7a474ad2faf2cce64
CenturionTheMan <dziedziakmichal0@gmail.com>
2024-05-03 16:10:05 +0200

    detection -> baby steps

--- Code/main.py
+++ Code/main.py
@@ -5,21 +5,19 @@ import cv2
 import torch
 from video_handler import VideoHandler
 from line_drawer import draw_parking_line
-from objects_detection import ObjectsDetector
+from objects_detection import YoloObjectsDetector, draw_boxes, SignsObjectsDetector
 import supervision as sv
 
 if __name__ == '__main__':
     video_handler = VideoHandler('../Videos/2024-03-27_13-15-35-front.mp4')
 
-    objects = {
+    yolo_objects = {
         2: ((255, 0, 0), 1), # car
         7: ((255, 0, 0), 1), # truck
         0: ((0, 255, 0), 1),  # person
-        11: ((0, 0, 255), 1)  # stop sign
     }
-    objects_detector = ObjectsDetector(objects)
-
-    #frame_size = (640, 480)
+    yolo_objects_detector = YoloObjectsDetector(yolo_objects)
+    sign_object_detector = SignsObjectsDetector()
 
     frame_rate = video_handler.capture.get(cv2.CAP_PROP_FPS)
     print(frame_rate)
@@ -28,7 +26,11 @@ if __name__ == '__main__':
     width = frame.shape[1]
 
     while frame is not None:
-        objects_detector.detect_and_mark_objects(frame, draw_on_th_frame=3)
+        detections = yolo_objects_detector.detect_objects(frame, draw_on_th_frame=2)
+        draw_boxes(frame, detections)
+
+        detections = sign_object_detector.detect_signs(frame=frame, draw_on_th_frame=2)
+        draw_boxes(frame, detections)
 
         # draw_parking_line(frame, pivot=(width * 0.15, 0), angle_deg=15, length=700, max_thickness=30, min_thickness=10, rgb=(255, 255, 255))
         # draw_parking_line(frame, pivot=(width * 0.85, 0), angle_deg=-15, length=700, max_thickness=30, min_thickness=10, rgb=(255, 255, 255))
--- Code/objects_detection.py
+++ Code/objects_detection.py
@@ -1,16 +1,34 @@
-import yolov5
 import numpy as np
 import torch
 import cv2
 from threading import Thread
 import torchvision
+#import yolov5
+from inference import get_model
+import supervision as sv
+from dataclasses import dataclass
 
 
-class ObjectsDetector:
+
+
+@dataclass
+class ObjectDetection:
+    object_name: str
+    bottom_left_corner: tuple[int, int]
+    top_right_corner: tuple[int, int]
+    color: tuple[int, int, int]
+    thickness: int
+
+def draw_boxes(frame, detections: list[ObjectDetection]):
+    for detection in detections:
+        cv2.rectangle(frame, detection.bottom_left_corner, detection.top_right_corner, detection.color, detection.thickness)
+
+
+class YoloObjectsDetector:
     def __init__(self, objects_to_detect: dict[int, tuple[tuple[int, int, int], int]]):
         # load pretrained model
         self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-        self.model.conf = 0.10  # NMS confidence threshold
+        self.model.conf = 0.20  # NMS confidence threshold
         self.model.iou = 0.45  # NMS IoU threshold
         self.model.agnostic = False  # NMS class-agnostic
         self.model.multi_label = False  # NMS multiple labels per box
@@ -27,12 +45,19 @@ class ObjectsDetector:
         print(f"cuda:{next(self.model.parameters()).is_cuda}")
 
         self.counter = 0
-        self.detected_objects = []
+        self.detected_objects = None
         self.objects_to_detect = objects_to_detect
         self.thread = None
 
-    def detect_and_mark_objects(self, frame, draw_on_th_frame=5):
-        if len(self.detected_objects) == 0:
+        self.label_decoder = {
+            0: "person",
+            2: "car",
+            7: "truck",
+            11: "stop sign",
+        }
+
+    def detect_objects(self, frame, draw_on_th_frame=5):
+        if self.detected_objects is None:
             self.__detect_objects(frame)
 
         if self.counter < draw_on_th_frame:
@@ -45,9 +70,7 @@ class ObjectsDetector:
         else:
             self.counter += 1
 
-        for label, x_min, y_min, x_max, y_max, conf in self.detected_objects:
-            color, thickness = self.objects_to_detect[label]
-            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color, thickness)
+        return self.detected_objects
 
     def __detect_objects(self, frame):
         results = self.model(frame)
@@ -57,6 +80,34 @@ class ObjectsDetector:
             if label in self.objects_to_detect:
                 # Draw bounding box
                 x_min, y_min, x_max, y_max, conf = map(int, res[:5])
-                detections.append((label, x_min, y_min, x_max, y_max, conf))
+                color, thickness = self.objects_to_detect[label]
+                obj = ObjectDetection(self.label_decoder[label], (x_min, y_min), (x_max, y_max), color, thickness)
+                detections.append(obj)
         self.detected_objects = detections
-        return detections
+
+
+class SignsObjectsDetector:
+    def __init__(self):
+        self.model = get_model(model_id="znaki-drogowe-w-polsce/15", api_key="1UHD3uECCOTgnJZg0Lh8")
+        self.model.confidence_threshold = 0.1
+        self.model.iou_threshold = 0.45
+        self.model.max_det = 10
+        self.model.agnostic = False
+        self.counter = 1
+        self.detected_objects = None
+
+    def detect_signs(self, frame, draw_on_th_frame=5):
+        if self.counter % draw_on_th_frame == 1:
+            results = self.model.infer(frame)
+            detections = sv.Detections.from_inference(results[0].dict(by_alias=True, exclude_none=True))
+            objects = []
+            for label, cords in zip(detections.data['class_name'], detections.xyxy):
+                obj = ObjectDetection(label, (int(cords[0]), int(cords[1])), (int(cords[2]), int(cords[3])), (0, 0, 255), 1)
+                objects.append(obj)
+            self.detected_objects = objects
+
+        if self.counter < draw_on_th_frame - 1:
+            self.counter += 1
+        else:
+            self.counter = 0
+        return self.detected_objects
